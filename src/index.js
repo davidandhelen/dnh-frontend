ea8172d40c6c4578bc2ceef386315b6519b82d75
helencho <helencho@ac.c4q.nyc>
2020-01-20 22:45:22 -0500

    install a bunch of things in order to get graphql working, still a work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import ApolloClient, { gql } from "apollo-boost";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -8,6 +10,28 @@ import rootReducer from "./reducers";
 import App from "./App";
 
 import "./style/reset.scss";
+
+const client = new ApolloClient({
+  uri: "https://davidandhelen.now.sh/"
+});
+
+// TODO: the below does not work!
+client
+  .query({
+    mutation: gql`
+      {
+        login(email: "elixirality@gmail.com", phone: "5162447969") {
+          token
+          user {
+            id
+            firstName
+            lastName
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 const store = createStore(rootReducer);
 
--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import { Heading1, BodyText } from "../../kit/typography";
+
 import css from "./Splash.module.scss";
 
 const Splash = () => {
   return (
     <div className={css.container}>
-      <h1 className={css.date}>10.10.2020</h1>
-      <h3 className={css.honoring}>David & Helen</h3>
+      <Heading1>David & Helen</Heading1>
+      <BodyText>October 10, 2020</BodyText>
     </div>
   );
 };
