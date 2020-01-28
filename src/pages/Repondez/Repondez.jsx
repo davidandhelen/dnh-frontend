c43c4f45968377d1db908a0b9324c766f04d7b34
helencho <helencho@ac.c4q.nyc>
2020-01-28 07:35:25 -0500

    use react final form for handling forms, instead of trying to do it on my own using hooks

--- src/pages/Repondez/Repondez.jsx
+++ src/pages/Repondez/Repondez.jsx
@@ -1,57 +1,83 @@
 /* eslint-disable no-unused-vars */
 // @flow
 import React, { useState } from "react";
+import { Field, Form } from "react-final-form";
 
 import { Heading1, Heading2 } from "../../kit/typography";
 
 import Button from "../../kit/Button";
+import Input from "../../kit/Input";
 
 import PlusOne from "./PlusOne";
 
 import css from "./Repondez.module.scss";
 
 const Repondez = props => {
-  const onChange = event => {
-    // TODO: Set state for each input
-  };
-
-  const onSubmit = event => {
-    event.preventDefault();
+  const onSubmit = values => {
+    // console.log("Submitting form");
+    // console.log(values);
     // TODO: RSVP users here.
   };
 
-  const renderPlusOneSection = count => {
-    const sections = [];
-    for (let i = 0; i < count; i++) {
-      sections.push(<PlusOne count={i} key={i} onChange={onChange} />);
-    }
-    return sections;
-  };
+  // const renderPlusOneSection = count => {
+  //   const sections = [];
+  //   for (let i = 0; i < count; i++) {
+  //     sections.push(
+  //       <PlusOne
+  //         count={i}
+  //         key={i}
+  //         onChange={onChange}
+  //         updateGuest={updateGuest}
+  //       />
+  //     );
+  //   }
+  //   return sections;
+  // };
 
   const { user: { allowedPlusOnes } = {} } = props;
   const [isShowingGuestForm, toggleGuestForm] = useState(false);
-  const [form, updateForm] = useState({});
+  const [guest, updateGuest] = useState([]);
 
   return (
     <div className={css.container}>
       <Heading1>
         {props.user.firstName}, reserve your spot at the table.
       </Heading1>
-      {!!allowedPlusOnes && (
-        <>
-          <Heading2>
-            You have {allowedPlusOnes} guest{allowedPlusOnes > 1 ? "s " : " "}
-            to your name.
-          </Heading2>
-          <Button onClick={() => toggleGuestForm(true)}>
-            Enter guest information
-          </Button>
-        </>
-      )}
-      <form className={css.form} onSubmit={onSubmit}>
-        {isShowingGuestForm && renderPlusOneSection(allowedPlusOnes)}
-        <Button type="submit">Reserve</Button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        {props => (
+          <form onSubmit={props.handleSubmit}>
+            <Field name="firstName" placeholder="First Name">
+              {props => (
+                <Input
+                  name={props.input.name}
+                  onChange={props.input.onChange}
+                  placeholder={props.input.placeholder}
+                  value={props.input.value}
+                />
+              )}
+            </Field>
+            <Field name="lastName">
+              {props => (
+                <Input
+                  name={props.input.name}
+                  onChange={props.input.onChange}
+                  value={props.input.value}
+                />
+              )}
+            </Field>
+            <Field name="phone">
+              {props => (
+                <Input
+                  name={props.input.name}
+                  onChange={props.input.onChange}
+                  value={props.input.value}
+                />
+              )}
+            </Field>
+            <Button type="submit">Reserve</Button>
+          </form>
+        )}
+      </Form>
     </div>
   );
 };
