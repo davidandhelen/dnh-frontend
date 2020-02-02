aea15e01c09529d58b3f0d0fe1c6e1df53fae810
helencho <helencho@ac.c4q.nyc>
2020-02-02 13:20:40 -0500

    nav version 1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
 // @flow
 import React from "react";
 
+import { Heading1, BodyText } from "../../kit/typography";
+
+import css from "./Home.module.scss";
+
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <div className={css.container}>
+      <Heading1>David Shin & Helen Cho</Heading1>
+      <BodyText>October 10, 2020</BodyText>
+      <BodyText>Moakyang Presbyterian Church</BodyText>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Wedding/Wedding.jsx
@@ -0,0 +1,21 @@
+// @flow
+import React from "react";
+
+import { Heading1, Heading2, BodyText } from "../../kit/typography";
+
+const Wedding = () => {
+  return (
+    <div>
+      <Heading1>Moakyang Presbyterian Church</Heading1>
+      <BodyText>12-25 Clintonville Street</BodyText>
+      <BodyText>Whitestone, NY 11357</BodyText>
+
+      <Heading2>Ceremony</Heading2>
+      <BodyText>Doors open 3:30PM</BodyText>
+
+      <Heading2>Reception</Heading2>
+    </div>
+  );
+};
+
+export default Wedding;
