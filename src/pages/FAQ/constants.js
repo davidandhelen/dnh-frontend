56bd6d292a4c62ce42a54add78cd27c3431a313a
helencho <helencho@ac.c4q.nyc>
2020-04-15 21:07:42 -0400

    create an FAQ component for /faq page and start a list of question and answers

---
+++ src/pages/FAQ/constants.js
@@ -0,0 +1,60 @@
+export const FAQS = [
+  {
+    answer:
+      "We're getting married in a speakeasy-turned-synagogue-turned-event-venue space called the Beverley Social Club. The building is rich in history, from the Prohibition to gentrification. It's located in Brooklyn.",
+    question: "Where is the ceremony taking place?"
+  },
+  {
+    answer: "Same venue as the ceremony.",
+    question: "Where is the reception taking place?"
+  },
+
+  {
+    answer: "No.",
+    question: "Is there a parking lot?"
+  },
+  {
+    answer:
+      "The venue is located in a residential area of Brooklyn. Think houses with full driveways. There will be plenty of street parking for our guests. We do encourage taking public transit to get to the venue, especially if you'll be drinking.",
+    question: "Where can I park?"
+  },
+  {
+    answer: "Transit directions coming soon...",
+    question: "How do I get there by public transit?"
+  },
+  {
+    answer: "If the kid is yours, yes.",
+    question: "Can I bring kids? Babies?"
+  },
+  {
+    answer:
+      "A variety of Korean food will be served, buffet style. The entrees and sides range from rice to vegetables to seafood to red meat.",
+    question: "What kind of food will be served?"
+  },
+  {
+    answer: "Depends on who you are.",
+    question: "Will there be alcohol?"
+  },
+  {
+    answer:
+      "There is no registry! In lieu of a registry, we will be accepting cash gifts at the door, as is customary for Korean American weddings.",
+    question: "Where is your registry?"
+  },
+  {
+    answer:
+      "No photos please! We have a professional weddding photographer to capture moments throughout the day. Enjoy the moment and be with us.",
+    question: "Can I take photos?"
+  },
+  {
+    answer: "No. Not about that.",
+    question: "Do you have an Instagram hashtag?"
+  },
+  {
+    answer: "We're closely watching the impacts of the coronavirus.",
+    question: "Are you worried about Covid19?"
+  },
+  {
+    answer: "It's okay, we understand.",
+    question: "I'm worried I won't be able to make it, due to Covid19."
+  }
+];
