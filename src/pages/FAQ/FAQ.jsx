875664501253399e492bc424d927e22e6e985032
helencho <helencho@ac.c4q.nyc>
2020-04-18 11:17:12 -0400

    update faq questions and answers

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -10,7 +10,7 @@ import css from "./FAQ.module.scss";
 
 const FAQ = () => (
   <div className={css.container}>
-    <Heading1 className={css.heading}>Frequently asked questions</Heading1>
+    <Heading1 className={css.heading}>Frequently Asked Questions</Heading1>
     {FAQS.map((questionSet, index) => (
       <div className={css.wrapper} key={`faq_${index}`}>
         <Heading2>{questionSet.question}</Heading2>
--- src/pages/FAQ/constants.js
+++ src/pages/FAQ/constants.js
@@ -1,7 +1,10 @@
 export const FAQS = [
   {
     answer:
-      "We're getting married in a speakeasy-turned-synagogue-turned-event-venue space called the Beverley Social Club. The building is rich in history, from the Prohibition to gentrification. It's located in Brooklyn.",
+      "We are getting married in a speakeasy-turned-synagogue-turned-event-venue space called \
+      the Beverley Social Club. The building is rich in history, having served patrons during \
+      the Prohibition era to living a life as a house of worship. We're excited to travel back in time to the Roaring Twenties, \
+      as all the furniture have survived since 1920's. The venue located in Flatbush, Brooklyn.",
     question: "Where is the ceremony taking place?"
   },
   {
@@ -15,7 +18,8 @@ export const FAQS = [
   },
   {
     answer:
-      "The venue is located in a residential area of Brooklyn. Think houses with full driveways. There will be plenty of street parking for our guests. We do encourage taking public transit to get to the venue, especially if you'll be drinking.",
+      "The venue is located in a residential area of Brooklyn. Think houses with full driveways. \
+      There will be plenty of street parking for our guests. We do encourage taking public transit to get to the venue, especially if you'll be drinking.",
     question: "Where can I park?"
   },
   {
@@ -23,38 +27,35 @@ export const FAQS = [
     question: "How do I get there by public transit?"
   },
   {
-    answer: "If the kid is yours, yes.",
+    answer:
+      "If the kid is yours and if they have been accounted for in the invite, yes.",
     question: "Can I bring kids? Babies?"
   },
   {
     answer:
-      "A variety of Korean food will be served, buffet style. The entrees and sides range from rice to vegetables to seafood to red meat.",
+      "A variety of Korean dishes will be served, buffet style. The entrees and sides range from rice to vegetables to seafood to red meats.",
     question: "What kind of food will be served?"
   },
   {
-    answer: "Depends on who you are.",
-    question: "Will there be alcohol?"
-  },
-  {
     answer:
-      "There is no registry! In lieu of a registry, we will be accepting cash gifts at the door, as is customary for Korean American weddings.",
+      "We do not have a wedding registry. In lieu of physical gifts, which is a very Western thing to do, \
+      we will be accepting cash gifts at the door, as is customary for Korean American weddings. Confused? Read about it here.",
     question: "Where is your registry?"
   },
   {
     answer:
-      "No photos please! We have a professional weddding photographer to capture moments throughout the day. Enjoy the moment and be with us.",
+      "No photos. We have a professional wedding photographer to capture moments throughout the day. Enjoy the moment and be with us.",
     question: "Can I take photos?"
   },
   {
-    answer: "No. Not about that.",
+    answer:
+      "No. The entire world doesn't need to know about two private families merging into one.",
     question: "Do you have an Instagram hashtag?"
   },
   {
-    answer: "We're closely watching the impacts of the coronavirus.",
-    question: "Are you worried about Covid19?"
-  },
-  {
-    answer: "It's okay, we understand.",
-    question: "I'm worried I won't be able to make it, due to Covid19."
+    answer:
+      "We're closely watching the impact of the virus, with current plans to hold a wedding ceremony as scheduled. \
+      If plans change due to new developments arising from the pandemic, we will reach out to let you know.",
+    question: "Are you worried about covid-19?"
   }
 ];
