909537d5118faf79e2b404128d4e380fb412c6fe
Helen <helen@hioscar.com>
2020-06-27 14:26:05 -0400

    add background image to the login form

--- src/kit/Button.jsx
+++ src/kit/Button.jsx
@@ -1,11 +1,18 @@
 // @flow
+import classNames from "classnames";
 import React from "react";
 
 import css from "./Button.module.scss";
 
-const Button = ({ children, ...rest }) => {
+const Button = ({ children, theme, ...rest }) => {
+  const classes = classNames({
+    [css.button]: true,
+    [css.button_dark]: true,
+    [css.button_light]: theme === "light"
+  });
+
   return (
-    <button className={css.button} {...rest}>
+    <button className={classes} {...rest}>
       {children}
     </button>
   );
